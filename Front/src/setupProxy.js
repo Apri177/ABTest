8f377a7ca4dc969c4425ac28110b8ff332d87008
april_07 <81811034+Apri177@users.noreply.github.com>
2023-10-27 14:47:24 +0900

    docker port

--- Front/src/setupProxy.js
+++ Front/src/setupProxy.js
@@ -4,7 +4,11 @@ module.exports = function(app) {
     app.use(
         '/api',
         createProxyMiddleware({
-            target: 'http://localhost:8080',
+            // docker port 17047
+            target: 'http://localhost:17047',
+
+            // local port 8080
+            // target: 'http://localhost:8080',
             changeOrigin: true,
         })
     )
